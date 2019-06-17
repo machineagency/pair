5705080f30c2e39f8c9d81e11fdfd480dd22c965
Jasper O'Leary <jhaazpr@gmail.com>
2019-06-17 12:05:59 -0700

    fix dot showing

--- projection.py
+++ projection.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
 
-def dot_at(x, y, img_size):
-    img = np.zeros(img_size, np.uint8)
-    radius = 100
+GRID_IMG_SIZE = (400, 400)
+
+def dot_at(x, y, img=None):
+    if img is None:
+        img_size_three_channel = GRID_IMG_SIZE + (3,)
+        img = np.zeros(img_size_three_channel, np.float32)
+    radius = 1
     dot_color = (0, 0, 255)
-    # img_dot = cv2.circle(img, (x, y), radius, dot_color, -1)
-    # return img_dot
-    return cv2.circle(img,(247,63), 63, (0,0,255), -1)
+    return cv2.circle(img, (x, y), radius, dot_color, -1)
 
 def text():
     # TODO: clean up
