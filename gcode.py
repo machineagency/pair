6f5ab55cca2261f469e368d745ab40143b9a7c65
Jasper O'Leary <jhaazpr@gmail.com>
2019-06-29 19:49:44 -0700

    gcode tweak

--- gcode.py
+++ gcode.py
@@ -1,7 +1,7 @@
-DEFAULT_FEED_RATE = 500
+DEFAULT_FEED_RATE = 2000
 
 def travel(x, y, z=None):
     if z:
-        return "G1 X{0} Y{1} Z{2} F{3}".format(x, y, z, DEFAULT_FEED_RATE)
-    return "G1 X{0} Y{1} F{2}".format(x, y, DEFAULT_FEED_RATE)
+        return "G1 X{0} Y{1} Z{2} F{3}\r\n".format(x, y, z, DEFAULT_FEED_RATE)
+    return "G1 X{0} Y{1} F{2}\r\n".format(x, y, DEFAULT_FEED_RATE)
 
