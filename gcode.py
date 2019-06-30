fafd3cecb22a630c7f7c60b4a5b33547d0ca52d4
Jasper O'Leary <jhaazpr@gmail.com>
2019-06-29 19:10:23 -0700

    default feed rate

--- gcode.py
+++ gcode.py
@@ -1,5 +1,7 @@
+DEFAULT_FEED_RATE = 500
+
 def travel(x, y, z=None):
     if z:
-        return "G1 X{0} Y{1} Z{2}".format(x, y, z)
-    return "G1 X{0} Y{1}".format(x, y)
+        return "G1 X{0} Y{1} Z{2} F{3}".format(x, y, z, DEFAULT_FEED_RATE)
+    return "G1 X{0} Y{1} F{2}".format(x, y, DEFAULT_FEED_RATE)
 
