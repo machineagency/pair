c78fa9fb5bbf01c35df488af6108749fa0ab2baf
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-06 17:46:44 -0800

    add main file stub

---
+++ light_space/pair.py
@@ -0,0 +1,3 @@
+# TODO: main file for parsing, projecting, sensing, and machine control.
+# takes in a pair file as input
+# TODO: bypass parsing with canned data for line example and create projection
