38f398ff70cc28e46c2c83eee1d3740699d0ab0a
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-07 12:58:12 -0800

    change projection API

--- light_space/projection.py
+++ light_space/projection.py
@@ -3,13 +3,13 @@ import numpy as np
 
 GRID_IMG_SIZE = (400, 400)
 
-def dot_at(x, y, img=None):
+def dot_at(pt, img=None):
     if img is None:
         img_size_three_channel = GRID_IMG_SIZE + (3,)
         img = np.zeros(img_size_three_channel, np.float32)
     radius = 1
     dot_color = (0, 0, 255)
-    return cv2.circle(img, (x, y), radius, dot_color, -1)
+    return cv2.circle(img, pt, radius, dot_color, -1)
 
 def line_from_to(p0, p1, img=None):
     if img is None:
@@ -19,7 +19,7 @@ def line_from_to(p0, p1, img=None):
     dot_color = (0, 0, 255)
     return cv2.line(p0, p1, line_color, thickness, CV_AA)
 
-def text_at(text, x, y, img=None):
+def text_at(text, pt, img=None):
     if img is None:
         img_size_three_channel = GRID_IMG_SIZE + (3,)
         img = np.zeros(img_size_three_channel, np.float32)
@@ -27,6 +27,6 @@ def text_at(text, x, y, img=None):
     font_scale = 1
     font_color = (255, 255, 255)
     thickness = 1
-    return cv2.putText(img, text, (x, y), font, font_scale, font_color, \
+    return cv2.putText(img, text, pt, font, font_scale, font_color, \
                        thickness, cv2.LINE_AA)
 
