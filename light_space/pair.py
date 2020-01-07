bc36e84e2284d10b49ecc0116bb990c48d416d5f
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-07 13:17:41 -0800

    canvas basic example

--- light_space/pair.py
+++ light_space/pair.py
@@ -2,6 +2,7 @@
 # takes in a pair file as input
 # TODO: bypass parsing with canned data for line example and create projection
 import cv2
+import numpy as np
 from machine import Machine
 import projection
 
@@ -9,6 +10,26 @@ def rescale_frame(frame, wpercent=130, hpercent=130):
     width = int(frame.shape[1] * wpercent / 100)
     height = int(frame.shape[0] * hpercent / 100)
     return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+def run_canvas_loop():
+    GRID_IMG_SIZE = (400, 400)
+    img_size_three_channel = GRID_IMG_SIZE + (3,)
+    img = np.zeros(img_size_three_channel, np.float32)
+    cv2.imshow("Projection", img)
+
+    while True:
+        pressed_key = cv2.waitKey(1)
+
+        # Close window on Escape keypress
+        if pressed_key == 27:
+            break
+
+        # Example color print for one frame only
+        elif pressed_key > 0 and pressed_key < 0x10FFFF:
+            projection.text_at(chr(pressed_key), (100, 100), img)
+
+        cv2.imshow("Projection", img)
+    cv2.destroyAllWindows()
 
 def run_camera_loop():
     capture = cv2.VideoCapture(0)
@@ -31,7 +52,7 @@ def run_camera_loop():
     capture.release()
 
 def main():
-    run_camera_loop()
+    run_canvas_loop()
 
 if __name__ == '__main__':
     main()
