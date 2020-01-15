9132c71a46f2d10238ba6eb49069d37c1066f5b8
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-14 18:26:02 -0800

    print machine output even if not dry

--- light_space/machine.py
+++ light_space/machine.py
@@ -10,27 +10,27 @@ class Machine:
             self.ad.connect()
 
     def pen_up(self):
-        if self.dry:
-            return 'pen_up'
-        self.ad.penup()
+        if not self.dry:
+            self.ad.penup()
+        return 'pen_up'
 
     def pen_down(self):
-        if self.dry:
-            return 'pen_down'
-        self.ad.pendown()
+        if not self.dry:
+            self.ad.pendown()
+        return 'pen_down'
 
     def line(self, pt):
-        if self.dry:
-            return f'line {pt}'
-        self.ad.lineto(pt[0], pt[1])
+        if not self.dry:
+            self.ad.lineto(pt[0], pt[1])
+        return f'line {pt}'
 
     def travel(self, pt):
-        if self.dry:
-            return f'travel {pt}'
-        self.ad.moveto(pt[0], pt[1])
+        if not self.dry:
+            self.ad.moveto(pt[0], pt[1])
+        return f'travel {pt}'
 
     def disconnect(self):
-        if self.dry:
-            return 'disconnect'
-        self.ad.disconnect()
+        if not self.dry:
+            self.ad.disconnect()
+        return 'disconnect'
 
