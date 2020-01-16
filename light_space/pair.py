b6b072729be3e2cf23501b1c262b2fe691bb79a0
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-16 13:31:55 -0800

    remove old camera code

--- light_space/pair.py
+++ light_space/pair.py
@@ -169,31 +169,6 @@ def run_canvas_loop():
         machine.return_to_origin()
         machine.disconnect()
 
-def run_camera_loop():
-    def rescale_frame(frame, wpercent=130, hpercent=130):
-        width = int(frame.shape[1] * wpercent / 100)
-        height = int(frame.shape[0] * hpercent / 100)
-        return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
-
-    capture = cv2.VideoCapture(0)
-
-    while capture.isOpened():
-        pressed_key = cv2.waitKey(1)
-        _, frame = capture.read()
-
-        # Close window on Escape keypress
-        if pressed_key == 27:
-            break
-
-        # Example color print for one frame only
-        elif pressed_key > 0 and pressed_key < 0x10FFFF:
-            projection.text_at(chr(pressed_key), (100, 100), frame)
-
-        cv2.imshow("Live Feed", rescale_frame(frame, 80, 80))
-
-    cv2.destroyAllWindows()
-    capture.release()
-
 def main():
     run_canvas_loop()
 
