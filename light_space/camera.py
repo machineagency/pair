e3d9b13bf2d698ce8274630e10def207cb7381be
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-17 18:05:45 -0800

    image display modularized

--- light_space/camera.py
+++ light_space/camera.py
@@ -1,12 +1,15 @@
 import cv2
 
-img_raw = cv2.imread('./test_images/work_env_lines.jpg')
+def process_image(img_path):
+    img_raw = cv2.imread(img_path)
+    img_gray = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
+    return img_gray
 
-window_name = 'Camera'
-cv2.namedWindow(window_name)
-cv2.imshow(window_name, img_raw)
-
-def run_camera_loop():
+def run_camera_loop(img_path):
+    img = process_image(img_path)
+    window_name = 'Camera'
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, img)
     while True:
         pressed_key = cv2.waitKey(1)
 
@@ -16,7 +19,7 @@ def run_camera_loop():
     cv2.destroyAllWindows()
 
 def main():
-    run_camera_loop()
+    run_camera_loop('./test_images/work_env_lines.jpg')
 
 if __name__ == '__main__':
     main()
