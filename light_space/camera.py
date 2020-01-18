1b90ace0ec68e38ba36bc08d8c427e432cb8f942
Jasper O'Leary <jhaazpr@gmail.com>
2020-01-18 11:26:51 -0800

    very basic contouring need to remove noise and double lines

--- light_space/camera.py
+++ light_space/camera.py
@@ -32,6 +32,10 @@ def run_camera_loop(img_path):
     window_name = 'Camera'
     cv2.namedWindow(window_name)
 
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE,\
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img_orig, contours, -1, (0, 255, 0), 1)
+
     cv2.imshow(window_name, img_orig)
     while True:
         pressed_key = cv2.waitKey(1)
