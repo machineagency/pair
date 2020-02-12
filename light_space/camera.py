ff9ca958627ce7cccaa4c3b6faa03b514f80f1d2
Jasper O'Leary <jhaazpr@gmail.com>
2020-02-11 16:32:57 -0800

    WIP camera working w video, see todos

--- light_space/camera.py
+++ light_space/camera.py
@@ -2,12 +2,11 @@ import cv2
 import numpy as np
 from scipy.spatial.distance import euclidean as dist
 
-def process_image(img_path):
-    img = cv2.imread(img_path)
+def process_image(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.GaussianBlur(img, (11, 11), 1, 1)
+    # img = cv2.GaussianBlur(img, (11, 11), 1, 1)
     # _, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
-    img = cv2.Canny(img, 200, 300)
+    # img = cv2.Canny(img, 200, 300)
     return img
 
 def draw_hough_lines(edge_img, out_img):
@@ -110,35 +109,32 @@ def transform_contour_with_h(contour, h):
     return trans.astype(np.int32)
 
 def run_camera_loop(img_path):
-    PROJ_SCREEN_SIZE_HW = (720, 1280)
-    img_orig = cv2.imread(img_path)
-    img = process_image(img_path)
-    window_name = 'Camera'
-    cv2.namedWindow(window_name)
-    contours = calc_contours(img)
-    work_env_contour = find_work_env_in_contours(contours)
-    cv2.drawContours(img_orig, [work_env_contour], 0, (0, 255, 0), 3)
-
-    work_env_homog = calc_work_env_homog(img_orig, work_env_contour, PROJ_SCREEN_SIZE_HW)
-    img_crop = cv2.warpPerspective(img_orig, work_env_homog, (PROJ_SCREEN_SIZE_HW[1],\
-                                   PROJ_SCREEN_SIZE_HW[0]))
-    img_crop_volatile = img_crop.copy()
-
-    cv2.imshow(window_name, img_orig)
-    # cv2.imshow("edges", img)
-    cv2.imshow("crop", img_crop)
-    curr_contour_idx = 0
-    contours = decimate_contours(contours)
-    MIN_CONTOUR_LEN = 100
-    min_length_lambda = lambda c: cv2.arcLength(c, closed=True) > MIN_CONTOUR_LEN
-    contours = list(filter(min_length_lambda, contours))
-    print(f'Num contours: {len(contours)}')
+    c = Camera()
+    cv2.imshow('orig', c.img_orig)
 
     while True:
         pressed_key = cv2.waitKey(1)
 
         if pressed_key == 27:
             break
+
+        if pressed_key == ord('v'):
+            cv2.destroyAllWindows()
+            img = c._read_video_image()
+            img_raw = img.copy()
+            img = c._process_image(img)
+            try:
+                c.calc_candidate_contours((18, 28))
+                print(len(c.candidate_contours))
+                # TODO: figure out homography with camera -> projector dimensions
+                # work_env_homog = calc_work_env_homog(img_raw, c.work_env_contour, PROJ_SCREEN_SIZE_HW)
+                # img_crop = cv2.warpPerspective(img_raw, work_env_homog, (PROJ_SCREEN_SIZE_HW[1],\
+                #                   PROJ_SCREEN_SIZE_HW[0]))
+                cv2.drawContours(img_raw, c.candidate_contours, -1, (255, 0, 0), 1)
+                cv2.imshow('contours', img_raw)
+                cv2.imshow('edges', img)
+            except ValueError:
+                print('Found no rectangle')
 
         if pressed_key == ord('n'):
             img_crop_volatile = img_crop.copy()
@@ -158,24 +154,34 @@ class Camera:
         self.PROJ_SCREEN_SIZE_HW = (720, 1280)
         self.CM_TO_PX = 37.7952755906
         self.MIN_CONTOUR_LEN = 100
-        self.path = 'test_images/shapes.png'
+        self.path = 'test_images/form.png'
         self.img_orig = cv2.imread(self.path)
         self.contours = []
+        self.work_env_contour = None
+        self.video_capture = cv2.VideoCapture(0)
 
-    def _process_image(self):
-        img = cv2.imread(self.path)
+    def _process_image(self, img):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = cv2.GaussianBlur(img, (11, 11), 1, 1)
         # _, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
         img = cv2.Canny(img, 50, 80)
         return img
 
+    def _load_file_image(self):
+        return cv2.imread(self.path)
+
+    def _read_video_image(self):
+        ret, frame = self.video_capture.read()
+        return frame
+
     def calc_candidate_contours(self, envelope_hw):
-        img = self._process_image()
+        img = self._read_video_image()
+        img = self._process_image(img)
         contours = calc_contours(img)
         work_env_contour = find_work_env_in_contours(contours)
         envelope_hw_px = (round(envelope_hw[0] * self.CM_TO_PX),\
                           round(envelope_hw[1] * self.CM_TO_PX))
+        # TODO: this works with img_orig but we shouldn't be using it
         work_env_homog = calc_work_env_homog(self.img_orig, work_env_contour,\
                                              envelope_hw_px)
         decimated_contours = decimate_contours(contours)
@@ -186,6 +192,7 @@ class Camera:
         trans_contours = list(map(lambda c: transform_contour_with_h(c,\
                                 work_env_homog), culled_contours))
         self.contours = trans_contours
+        self.work_env_contour = work_env_contour
 
     @property
     def candidate_contours(self):
