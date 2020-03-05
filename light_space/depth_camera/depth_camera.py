944d2202e46a75a292108af8e3ac3eb5613ff955
Jasper O'Leary <jhaazpr@gmail.com>
2020-03-04 16:54:44 -0800

    stacking sobel gradients

--- light_space/depth_camera/depth_camera.py
+++ light_space/depth_camera/depth_camera.py
@@ -4,7 +4,6 @@ import cv2
 
 class DepthCamera():
     def __init__(self):
-        self.pipeline = self.init_pipeline()
         self.pipeline = rs.pipeline()
         self.config = rs.config()
         self.config.enable_stream(rs.stream.depth, 640, 480,\
@@ -13,8 +12,13 @@ class DepthCamera():
                                     rs.format.bgr8, 30)
         self.profile = self.pipeline.start(self.config)
 
-    def init_pipeline(self):
-        pass
+    def smooth_image(self, img):
+        return cv2.GaussianBlur(img, (3, 3), 1, 1)
+
+    def compute_sobel_gradients(self, depth_img):
+        sobel_x = cv2.Sobel(depth_img, cv2.CV_64F, 1, 0, ksize=7)
+        sobel_y = cv2.Sobel(depth_img, cv2.CV_64F, 0, 1, ksize=7)
+        return (sobel_x, sobel_y)
 
     def test(self):
         try:
@@ -25,15 +29,17 @@ class DepthCamera():
                     continue
                 depth_image = np.asanyarray(depth_frame.get_data())
                 depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+                sobel_x, sobel_y = self.compute_sobel_gradients(depth_image)
+                sobel_images = np.hstack((sobel_x, sobel_y))
                 cv2.namedWindow('depth', cv2.WINDOW_AUTOSIZE)
-                cv2.imshow('depth', depth_colormap)
+                cv2.imshow('depth', sobel_images)
                 key = cv2.waitKey(1)
                 # Press esc or 'q' to close the image window
                 if key & 0xFF == ord('q') or key == 27:
                     cv2.destroyAllWindows()
                     break
         finally:
-            pipeline.stop()
+            self.pipeline.stop()
 
 if __name__ == '__main__':
     dc = DepthCamera()
