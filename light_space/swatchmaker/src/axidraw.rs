8ff3415e905bd4010b0c9d8a596607088c6de89c
Jasper O'Leary <jhaazpr@gmail.com>
2021-05-26 15:31:47 -0700

    axidraw wrapper stub

---
+++ light_space/swatchmaker/src/axidraw.rs
@@ -0,0 +1,18 @@
+use std::process::Command;
+
+pub struct Axidraw {
+    name: String
+}
+
+impl Axidraw {
+    pub fn new() -> Self {
+        Axidraw { name: "axidraw" }
+    }
+    pub fn plot_svg_graph(svg: &SvgGraph) {
+    }
+    pub fn plot_svg_file(filename: &String) {
+    }
+}
+
+pub fn test() {
+}
