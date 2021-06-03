aeb131a80a8dd8bad950633439d8758d2cb4425e
Jasper O'Leary <jhaazpr@gmail.com>
2021-06-03 12:39:57 -0700

    iterate through filenames in vector directory

--- light_space/toolpath_collection.py
+++ light_space/toolpath_collection.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2
+import cv2, os
 from loader import Loader
 import projection
 
@@ -11,17 +11,25 @@ class ToolpathCollection:
         self.directory_rasters = 'images/rasters/'
         self.bitmap = np.zeros(self.BITMAP_HW_PX + (3,))
 
+    @property
+    def width(self):
+        return self.BITMAP_HW_PX[1]
+
+    def process_click_at_pt(self, pt):
+        # TODO: load the toolpath that was clicked on
+        print('NYI')
+
     def render_vectors(self):
         gutter_px = 25
         box_width = self.BITMAP_HW_PX[1]
         box_height = round(box_width * 0.75)
         overlay = np.zeros(self.bitmap.shape)
-        for i in range(3):
+        for i, filename in enumerate(os.listdir(self.directory_vectors)):
             y_offset = i * (box_height + gutter_px)
             overlay = overlay + projection.rectangle_at( \
                     (0, y_offset), \
                     box_width, box_height, self.bitmap)
-            paths = self._loader.load_svg('images/secret/nadya-sig.svg')
+            paths = self._loader.load_svg(self.directory_vectors + filename)
             trans_mat = np.array([[1, 0, 0], [0, 1, y_offset]])
             trans_paths = [cv2.transform(p, trans_mat) for p in paths]
             cv2.polylines(overlay, trans_paths, False, (0, 0, 255), 1)
