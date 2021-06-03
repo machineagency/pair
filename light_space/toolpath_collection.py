9e5494248ead5c2219bd01fea4a290372bd34a75
Jasper O'Leary <jhaazpr@gmail.com>
2021-06-03 12:14:11 -0700

    wip loading vectors

--- light_space/toolpath_collection.py
+++ light_space/toolpath_collection.py
@@ -1,10 +1,12 @@
 import numpy as np
+import cv2
 from loader import Loader
 import projection
 
 class ToolpathCollection:
     def __init__(self):
         self.BITMAP_HW_PX = (720, 200)
+        self._loader = Loader()
         self.directory_vectors = 'images/vectors/'
         self.directory_rasters = 'images/rasters/'
         self.bitmap = np.zeros(self.BITMAP_HW_PX + (3,))
@@ -13,12 +15,17 @@ class ToolpathCollection:
         gutter_px = 25
         box_width = self.BITMAP_HW_PX[1]
         box_height = round(box_width * 0.75)
-        box_overlay = np.zeros(self.bitmap.shape)
+        overlay = np.zeros(self.bitmap.shape)
         for i in range(3):
-            box_overlay = box_overlay + projection.rectangle_at( \
-                    (0, i * (box_height + gutter_px)), \
+            y_offset = i * (box_height + gutter_px)
+            overlay = overlay + projection.rectangle_at( \
+                    (0, y_offset), \
                     box_width, box_height, self.bitmap)
-        self.bitmap = self.bitmap + box_overlay
+            paths = self._loader.load_svg('images/secret/nadya-sig.svg')
+            trans_mat = np.array([[1, 0, 0], [0, 1, y_offset]])
+            trans_paths = [cv2.transform(p, trans_mat) for p in paths]
+            cv2.polylines(overlay, trans_paths, False, (0, 0, 255), 1)
+        self.bitmap = self.bitmap + overlay
 
     def add_bitmap_to_projection(self, proj):
         self.render_vectors()
