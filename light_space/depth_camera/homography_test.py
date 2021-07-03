5add9b35f86051f34822a7fce237a0b0e8bc5d08
Jasper O'Leary <jhaazpr@gmail.com>
2021-07-02 17:29:42 -0700

    cp finds corner points

--- light_space/depth_camera/homography_test.py
+++ light_space/depth_camera/homography_test.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
-import cv2.aruco as aruco
+from cv2 import aruco
 import pyrealsense2 as rs
 import sys
+from scipy.spatial import distance
 
 cam_img_height = 1080
 cam_img_width = 1920
 proj_img_height = 900
 proj_img_width = 1440
+framerate = 30
 
 cv2.namedWindow('Projector',cv2.WINDOW_NORMAL)
 cv2.namedWindow('Camera',cv2.WINDOW_NORMAL)
@@ -29,17 +31,45 @@ proj_img[-aruco1.shape[0]-off:-off, off:aruco1.shape[1]+off] = aruco3
 proj_img[-aruco1.shape[0]-off:-off, -aruco1.shape[1]-off:-off] = aruco4
 
 cv2.imshow('Projector', proj_img)
-while True:
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-sys.exit(0)
+# while True:
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# Image processing definitions
+def get_roi_corner_pts(img):
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+    parameters =  aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+    if len(corners) != 4:
+        return []
+    return get_outermost_points(corners, img)
+
+def get_outermost_points(corners_arr_lst, img):
+    flat_corner_sets = [cset.reshape(4, 2) for cset in corners_arr_lst]
+    all_points = np.concatenate(flat_corner_sets)
+
+    img_height, img_width, _ = img.shape
+    abs_upper_left = np.array([0, 0])
+    abs_upper_right = np.array([0, img_width])
+    dists_upper_left = [distance.euclidean(pt, abs_upper_left) \
+                            for pt in all_points]
+    dists_upper_right = [distance.euclidean(pt, abs_upper_right) \
+                            for pt in all_points]
+
+    idx_upper_left = np.argmin(dists_upper_left)
+    idx_upper_right = np.argmin(dists_upper_right)
+    idx_lower_left = np.argmax(dists_upper_right)
+    idx_lower_right = np.argmax(dists_upper_left)
+
+    return [all_points[idx_upper_right], all_points[idx_upper_left], \
+            all_points[idx_lower_left], all_points[idx_lower_right]]
 
 # Start camera
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, img_width,\
-                       img_height, rs.format.bgr8, framerate)
+config.enable_stream(rs.stream.color, cam_img_width,\
+                       cam_img_height, rs.format.bgr8, framerate)
 profile = pipeline.start(config)
 while True:
         frames = pipeline.wait_for_frames()
@@ -47,7 +77,8 @@ while True:
         if not color_frame:
             continue
         color_image = np.asanyarray(color_frame.get_data())
-        video_writer.write(color_image)
-        cv2.imshow('feed', color_image)
-        if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imshow('Camera', color_image)
+        corner_points = get_roi_corner_pts(color_image)
+        print(corner_points)
+        if cv2.waitKey(200) & 0xFF == ord('q'):
             break
