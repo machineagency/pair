9973046e10f094335c04642e74ddf18ceba338a1
Jasper O'Leary <jhaazpr@gmail.com>
2021-08-06 15:29:22 -0700

    paper boilerplate

---
+++ light_space/client/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Make the paper scope global, by injecting it into window:
+paper.install(window);
+window.onload = function() {
+	// Setup directly from canvas id:
+	paper.setup('mainCanvas');
+	var path = new Path();
+	path.strokeColor = 'black';
+	var start = new Point(100, 100);
+	path.moveTo(start);
+	path.lineTo(start.add([ 200, -50 ]));
+	view.draw();
+}
