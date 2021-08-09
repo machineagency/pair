a1f29da1c73559d60056a034c4d6291412876e89
Jasper O'Leary <jhaazpr@gmail.com>
2021-08-09 11:33:33 -0700

    hit testing

--- light_space/client/main.js
+++ light_space/client/main.js
@@ -3,20 +3,55 @@
 class Tabletop {
     constructor() {
         this.project = paper.project;
-        this.workEnvelope = new WorkEnvelope(720, 480);
+        this.tool = new Tool();
+        this.workEnvelope = new WorkEnvelope(this, 720, 480);
+        this.toolpathCollection = new ToolpathCollection(this);
+        this.initMouseHandlers();
+    }
+
+    initMouseHandlers() {
+        const hitOptions = {
+            segments: true,
+            stroke: true,
+            fill: true,
+            tolerance: 5
+        };
+        this.tool.onMouseDown = (event, hitOptions) => {
+            // Clear existing selections
+            this.project.getItems({ selected: true }).forEach((item) => {
+                item.selected = false;
+            });
+            // Check hit for each preview (box + mini toolpath)
+            Object.values(this.toolpathCollection.collection)
+                .forEach((preview) => {
+                let hitResult = preview.hitTest(event.point);
+                if (hitResult) {
+                    preview.selected = true;
+                    // TODO: load real TP into canvas instead
+                }
+            });
+            // TODO: add handler for WE and selectable toolpaths
+        };
     }
 }
 
 class WorkEnvelope {
-    constructor(width, height) {
+    constructor(tabletop, width, height) {
+        this.tabletop = tabletop;
         this.width = width;
         this.height = height;
+        this.strokeWidth = 3;
         this.render();
+    }
+
+    get position() {
+        return new Point(this.strokeWidth, this.strokeWidth);
     }
 
     render() {
         let strokeWidth = 3;
-        let rect = new Rectangle(strokeWidth, strokeWidth, this.width, this.height);
+        let rect = new Rectangle(this.position.x, this.position.y,
+                                 this.width, this.height);
         let path = new Path.Rectangle(rect);
         path.strokeColor = 'red';
         path.strokeWidth = 3;
@@ -28,31 +63,53 @@ class Toolpath {
 }
 
 class ToolpathCollection {
-    static renderAnchor = { x: 25, y: 500 };
     constructor(tabletop) {
         this.tabletop = tabletop;
         tabletop.toolpathCollection = this;
+        this.previewSize = new Size(100, 75);
+        this.anchor = new Point(this.tabletop.workEnvelope.width
+            + this.previewSize.width / 2 + this.tabletop.workEnvelope.strokeWidth
+            + 20, this.previewSize.height / 2
+            + this.tabletop.workEnvelope.strokeWidth);
         this.collection = {};
-        this.tabletop.project.importSVG('./toolpaths/nadya-sig.svg', {
-            expandShapes: true,
-            insert: true,
-            onError: () => {
-                console.warn('Could not load an SVG');
-            },
-            onLoad: (item, svgString) => {
-                console.log(item.children);
-                item.children.forEach((child, idx) => {
-                    if (idx === 0) {
-                        // FIXME: this doesn't stop the bounding rectangle
-                        // from rendering
-                        child.visible = false;
-                    }
-                    else {
+        // TODO: eventually load this from the server
+        this.toolpathNames = [
+            'nadya-sig', 'box', 'wave'
+        ];
+        this.marginSize = 10;
+        this.initCollection();
+    }
+
+    initCollection() {
+        let origin = new Point(0, 0);
+        this.toolpathNames.forEach((tpName, tpIdx) => {
+            // TODO: make each box a Paper.Group
+            let box = new Path.Rectangle(origin, this.previewSize);
+            box.strokeColor = 'red';
+            box.strokeWidth = 2;
+            let currBoxPt = new Point(this.anchor.x, this.anchor.y + tpIdx
+                    * (this.previewSize.height + this.marginSize));
+            box.position = currBoxPt;
+            this.tabletop.project.importSVG(`./toolpaths/${tpName}.svg`, {
+                expandShapes: true,
+                insert: true,
+                onError: () => {
+                    console.warn('Could not load an SVG');
+                },
+                onLoad: (item, svgString) => {
+                    let scaleFactor = Math.min(this.previewSize.width
+                        / item.bounds.width, this.previewSize.height
+                        / item.bounds.height);
+                    item.scale(scaleFactor);
+                    item.position = currBoxPt;
+                    item.children.forEach((child, idx) => {
                         child.strokeColor = 'red';
                         child.strokeWidth = 1;
-                    }
-                });
-            }
+                    });
+                    let group = new Group([box, item]);
+                    this.collection[tpName] = group;
+                }
+            });
         });
     }
 }
@@ -70,7 +127,6 @@ const main = () => {
     // path.lineTo(start.add([ 200, -50 ]));
 
     let tabletop = new Tabletop();
-    let tpc = new ToolpathCollection(tabletop);
     window.tabletop = tabletop;
     // view.draw();
 };
