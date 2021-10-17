e705ce43a9ece0b1cd218c30dc81eca189d67a1c
Jasper O'Leary <jhaazpr@gmail.com>
2021-10-17 13:14:14 -0700

    add react component stubs

---
+++ light_space/client/livelit-windows.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+const e = React.createElement;
+
+class LivelitWindow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+};
+
+class GeometryGallery extends LivelitWindow {
+}
+
+class PointPicker extends LivelitWindow {
+}
