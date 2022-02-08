5c7d9c92b1629a53ed2e09e0f4183713f8a36838
Jasper O'Leary <jhaazpr@gmail.com>
2022-02-07 20:08:45 -0800

    add orbitcontrols code + type assets

--- client/lib/three-types/index.d.ts
+++ client/lib/three-types/index.d.ts
@@ -6,6 +6,8 @@
 // Minimum TypeScript Version: 3.6
 
 export * from './src/Three';
+// Add additional typing for OrbitControls
+export * from './examples/jsm/controls/OrbitControls';
 
 /*~ If this module is a UMD module that exposes a global variable 'myLib' when
  *~ loaded outside a module loader environment, declare that global here.
