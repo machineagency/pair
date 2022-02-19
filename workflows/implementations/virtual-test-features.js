043b3315363be6178e8094e716eec89fc8081eff
Jasper O'Leary <jhaazpr@gmail.com>
2022-02-18 22:12:46 -0800

    correct a workflow

--- workflows/implementations/virtual-test-features.js
+++ workflows/implementations/virtual-test-features.js
@@ -1,7 +1,8 @@
 let machine = new verso.Machine('axidraw');
-let tabletop = new verso.Tabletop();
+let tabletop = new verso.Tabletop(machine);
 let camera = new verso.Camera(tabletop);
-let mustache = new verso.Geometry('./toolpaths/mustache.svg');
+let mustache = new verso.Geometry(tabletop);
+mustache.loadFromFilepath('mustache.svg', './toolpaths/mustache.svg');
 let faceRegions = await camera.findFaceRegions();
 let faceCentroids = faceRegions.map(r => r.centroid);
 let toolpaths = faceCentroids.map(c => mustache.placeAt(c, tabletop));
