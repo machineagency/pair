d3e9ee94c5805121070945019b9a4d87666e40f1
Jasper O'Leary <jhaazpr@gmail.com>
2022-03-08 18:24:56 -0800

    remove obsolete default programs

--- client/program-pane.tsx
+++ client/program-pane.tsx
@@ -164,56 +164,6 @@ class ProgramPane extends React.Component<ProgramPaneProps, ProgramPaneState> {
     livelitRefs: React.RefObject<LivelitWindow>[];
     modulePaneRef: React.RefObject<ModulePane>;
 
-    defaultLinesSignature = [
-        'let signature = $geometryGallery;',
-        'let point = $pointPicker;',
-        'let toolpath = signature.placeAt(point);',
-        '// $toolpathTransformer signature',
-        '// $machineSynthesizer gigapan coinGeometry',
-        'machine.plot(toolpath);'
-    ];
-
-    defaultLinesMustacheExpanded = [
-        'let machine = new verso.Machine(\'axidraw\');',
-        'let tabletop = new verso.Tabletop();',
-        'tabletop = tabletop',
-        'let camera = new verso.Camera(tabletop);',
-        'let mustache = new verso.Geometry(\'./toolpaths/mustache.svg\')',
-        'let faceRegions = await camera.findFaceRegions();',
-        'let faceCentroids = faceRegions.map(r => r.centroid);',
-        'let toolpaths = faceCentroids.map(c => mustache.placeAt(c, tabletop));',
-        'toolpaths.forEach(toolpath => machine.plot(toolpath));'
-    ];
-
-    defaultLinesMustacheLiveLits = [
-        'let machine = new verso.Machine(\'axidraw\');',
-        'let tabletop = await $tabletopCalibrator(machine);',
-        'let camera = await $cameraCalibrator(tabletop);',
-        'let mustache = await $geometryGallery(machine);',
-        'let faceRegions = await $faceFinder(camera);',
-        'let mustachePoints = faceRegions.map(r => r.centroid.add(0, 0.25 * r.height));',
-        'let toolpaths = await Promise.all(mustachePoints.map(pt => mustache.placeAt(pt, tabletop)));',
-        // TODO: combine toolpaths into one object which will become one svg. in fact,
-        // maybe we should just place geometries, and then only form a single toolpath
-        // at the very end.
-        'toolpaths.forEach(toolpath => machine.plotToolpathOnTabletop(toolpath, tabletop));'
-    ];
-
-    defaultToolpathPreviewing = [
-        'let machine = new verso.Machine(\'axidraw\');',
-        '// TODO: pen up calibrator with preview',
-        'let tabletop = await $tabletopCalibrator(machine);',
-        'let camera = await $cameraCalibrator(tabletop);',
-        'let geometry = await $geometryGallery(machine, tabletop);',
-        '// TODO: use either camera or toolpath direct manipulator',
-        'let point = new verso.Point(mm(75), mm(25));',
-        'geometry.placeAt(point, tabletop);',
-        'let toolpath = await $camCompiler(machine, geometry);',
-        'let vizSpace = new verso.VisualizationSpace();',
-        'vizSpace = await $toolpathVisualizer(machine, toolpath, vizSpace);',
-        'console.log(vizSpace);'
-    ];
-
     constructor(props: ProgramPaneProps) {
         super(props);
         this.livelitRefs = [];
