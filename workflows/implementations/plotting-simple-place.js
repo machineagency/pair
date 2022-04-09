9cf4209d325b17aebe7572bbab76b4987a70704c
Jasper O'Leary <jhaazpr@gmail.com>
2022-04-09 16:06:55 -0700

    wf update

--- workflows/implementations/plotting-simple-place.js
+++ workflows/implementations/plotting-simple-place.js
@@ -1,8 +1,8 @@
 let machine = new verso.Machine('axidraw');
+machine = await $machineInitializer(machine);
 let tabletop = await $tabletopCalibrator(machine);
 let geometry = await $geometryGallery(tabletop);
 geometry = geometry.translate(mm(75), mm(25));
 let toolpath = await $axidrawDriver(machine, geometry);
 let vizSpace = await $toolpathVisualizer(machine, [toolpath]);
-await $projector(tabletop, vizSpace);
 await $dispatcher(machine, [toolpath]);
